1056a22e55a5392caeb83d6756b25b60c9633119
sammyoto <samgfales@gmail.com>
2025-12-01 20:33:21 -0700

    first commit

---
+++ day1/src/main.rs
@@ -0,0 +1,14 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn load_safe_rotation_sequence(filename: &str) -> {
+
+}
+
+fn decode_safe_rotation_sequence(sequence: &str) {
+    
+}
