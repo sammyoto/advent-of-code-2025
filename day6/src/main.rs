69ce56926df00bb4ba322015a94690e29fd79024
sammyoto <samgfales@gmail.com>
2025-12-10 14:14:27 -0700

    day 6 complete

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,13 +2,14 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let input = load_puzzle_input("src/test.txt");
-    let cephalopod_input = load_cephalopod_input("src/test.txt");
-    println!("Normal Input: {:?}", input.0);
-    println!("Cephalopod Input: {:?}", cephalopod_input[cephalopod_input.len()-1]);
+    let input = load_puzzle_input("src/puzzle_input.txt");
+    let cephalopod_input = load_cephalopod_input("src/puzzle_input.txt");
 
     let total = sum_all_functions(&input.0, &input.1);
-    println!("{}", total);
+    println!("Normal Total: {}", total);
+
+    let cephalopod_total = cephalopod_sum(&mut load_cephalopod_input("src/puzzle_input.txt"));
+    println!("Cephalopod Total: {}", cephalopod_total);
 }
 
 fn load_puzzle_input(filename: &str) -> (Vec<Vec<u64>>, Vec<char>) {
@@ -83,15 +84,98 @@ fn cephalopod_sum(numbers: &mut Vec<Vec<char>>) -> u64 {
     let mut total: u64 = 0;
 
     // Remove operators from numbers
-    let operators =  numbers.remove(numbers.len() - 1);
+    let mut operators =  numbers.remove(numbers.len() - 1);
+    // Remove spaces from operators
+    operators.retain(|x| *x != ' ');
 
+    // Loop through all rows
+    // Find columns with only ' '
+    // Record those indices
+    let mut separation_indices: Vec<usize> = Vec::new();
     // Col
-    for i in 0..numbers.len() {
-    // Row
-        for j in 0..numbers[0].len() {
-            
+    for i in 0..numbers[0].len() {
+        let mut is_separation_column = true;
+        // Row
+        for j in 0..numbers.len() {
+            if numbers[j][i] != ' ' {
+                is_separation_column = false;
+                break;
+            }
+        }
+
+        if is_separation_column {
+            separation_indices.push(i);
         }
     }
 
+    // Now we know where it's separated, we can now construct numbers
+    for i in 0..operators.len() {
+        let operator = operators[i];
+        let mut local_sum: u64 = 0;
+        // Start at 0 and end at 1st separation indicie
+        if i == 0 {
+            for j in 0..separation_indices[0] {
+                let mut column: Vec<char> = numbers.iter().map(|row| row[j]).collect();
+                column.retain(|x| *x != ' ');
+                let num = get_number_from_vector(&column);
+
+                if j == 0 {
+                    local_sum = num;
+                } else if operator == '*' {
+                    local_sum *= num;
+                } else {
+                    local_sum += num;
+                }
+            }
+        // Start at last separation indicie and end at end of numbers
+        } else if i == operators.len() - 1 {
+            for j in separation_indices[separation_indices.len() - 1] + 1..numbers[0].len() {
+                let mut column: Vec<char> = numbers.iter().map(|row| row[j]).collect();
+                column.retain(|x| *x != ' ');
+                let num = get_number_from_vector(&column);
+
+
+                if j == separation_indices[separation_indices.len() - 1] + 1 {
+                    local_sum = num;
+                } else if operator == '*' {
+                    local_sum *= num;
+                } else {
+                    local_sum += num;
+                }
+            }
+
+        // Start at i - 1 separation indicie and end at i separation indicie
+        } else {
+            for j in separation_indices[i - 1] + 1..separation_indices[i] {
+                let mut column: Vec<char> = numbers.iter().map(|row| row[j]).collect();
+                column.retain(|x| *x != ' ');
+                let num: u64 = get_number_from_vector(&column);
+
+                if j == separation_indices[i - 1] + 1 {
+                    local_sum = num;
+                } else if operator == '*' {
+                    local_sum *= num;
+                } else {
+                    local_sum += num;
+                }
+            }
+        }
+
+        total += local_sum;
+    }
+
     total
+}
+
+fn get_number_from_vector(number_vec: &Vec<char>) -> u64{
+    let mut number: u64 = 0;
+
+    for i in 0..number_vec.len() {
+        let digit: u64 = number_vec[i].to_digit(10).unwrap() as u64;
+        let digit = digit * (10_u64.pow((number_vec.len() - i - 1) as u32));
+        number += digit;
+        
+    }
+
+    number
 }
