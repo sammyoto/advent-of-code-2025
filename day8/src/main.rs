d18a9922d9ef603aca059dba9e1cefd68664c62e
sammyoto <samgfales@gmail.com>
2025-12-11 19:27:53 -0700

    starting day4

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,3 +1,14 @@
+struct coordinates {
+    x: u32,
+    y: u32,
+    z: u32
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+
+fn load_puzzle_input(filename: &str) -> Vec<coordinates> {
+    
 }
