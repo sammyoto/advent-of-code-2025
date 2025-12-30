43be58c3f80a28e4b70750177f34b99a51d60113
sammyoto <samgfales@gmail.com>
2025-12-30 10:34:43 -0700

    didn't get part 2 :(

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -40,8 +40,68 @@ fn find_largest_rectangle() -> usize {
 }
 
 fn find_largest_red_green_rectangle() -> usize {
-    let puzzle_input = load_puzzle_input("src/puzzle_input.txt");
+    let puzzle_input = load_puzzle_input("src/test.txt");
+    let puzzle_input_len = puzzle_input.len();
     let mut largest_area: usize = 0;
 
+    for i in 0..puzzle_input_len {
+        for j in i + 1..puzzle_input_len {
+            let tile_1 = &puzzle_input[i];
+            let tile_2 = &puzzle_input[j];
+
+            if check_valid_rectangle(tile_1[0], tile_1[1], tile_2[0], tile_2[1], &puzzle_input) {
+                let x_len = tile_1[0].abs_diff(tile_2[0]) + 1;
+                let y_len = tile_1[1].abs_diff(tile_2[1]) + 1;
+                let area = x_len * y_len;
+
+                println!("x1: {}, x2: {}, y1: {}, y2: {}, area: {}", tile_1[0], tile_2[0], tile_1[1], tile_2[1], area);
+
+                if area > largest_area {
+                    largest_area = area;
+                }
+            }
+        }
+    }
+
     largest_area
+}
+
+fn check_valid_rectangle(x_1: usize, y_1: usize, x_2: usize, y_2: usize, puzzle_input: &Vec<Vec<usize>>) -> bool {
+    // Follow puzzle input and create loops 
+    for i in 0..puzzle_input.len() {
+        let tile: &Vec<usize> = &puzzle_input[i];
+        
+        // Check if the tile is inside the area of the potential rectangle, if it is its invalid
+        if x_1 >= x_2 {
+            if y_1 >= y_2 {
+                if tile[0] > x_2 && tile[0] < x_1 && tile[1] > y_2 && tile[1] < y_1 {
+                    return false
+                } else {
+                    continue
+                }
+            } else {
+                if tile[0] > x_2 && tile[0] < x_1 && tile[1] > y_1 && tile[1] < y_2 {
+                    return false
+                } else {
+                    continue
+                }
+            }
+        } else {
+            if y_1 >= y_2 {
+                if tile[0] > x_1 && tile[0] < x_2 && tile[1] > y_2 && tile[1] < y_1 {
+                    return false
+                } else {
+                    continue
+                }
+            } else {
+                if tile[0] > x_1 && tile[0] < x_2 && tile[1] > y_1 && tile[1] < y_2 {
+                    return false
+                } else {
+                    continue
+                }
+            }
+        }
+    }
+
+    true
 }
